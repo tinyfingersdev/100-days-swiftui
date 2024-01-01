7015e24aecf8f13e3dce2eaee7e7f7528d828a80
Shannon Paige <github@bluewhomail.com>
2024-01-01 13:13:28 -0600

    day 19 challenge, had to create my own little ios app.

---
+++ MyTempConverter/MyTempConverter/MyTempConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTempConverterApp.swift
+//  MyTempConverter
+//
+//  Created by Shannon Paige on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MyTempConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
