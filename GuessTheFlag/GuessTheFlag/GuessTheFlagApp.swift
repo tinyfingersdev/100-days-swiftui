c960e17eb6a14e3bb4025f2a90b85ea6c7d03109
Shannon Paige <github@bluewhomail.com>
2024-01-03 20:17:27 -0600

    day 21 changes

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Shannon Paige on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
