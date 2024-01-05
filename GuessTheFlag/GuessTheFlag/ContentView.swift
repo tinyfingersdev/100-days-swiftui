c2e3db0dcf7048aa0c9761250e81bf7aedd2b758
Shannon Paige <github@bluewhomail.com>
2024-01-04 20:45:16 -0600

    final updates for the day 22 challenge

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -14,7 +14,11 @@ struct ContentView: View {
     @State private var correctAnswer = Int.random(in: 0...2)
     
     @State private var showingScore = false
+    @State private var showingGameEnd = false
     @State private var scoreTitle = ""
+    @State private var scoreMessage = ""
+    @State private var score = 0
+    @State private var questionNumber = 1
     
     var body: some View {
         ZStack {
@@ -59,7 +63,7 @@ struct ContentView: View {
                 Spacer()
                 Spacer()
                 
-                Text("Score: ???")
+                Text("Score: \(score)")
                     .foregroundStyle(.white)
                     .font(.title.bold())
                 
@@ -70,23 +74,65 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ????")
+            Text(scoreMessage)
+        }
+        .alert("Game completed!", isPresented: $showingGameEnd) {
+            Button("New Game", action: resetGame)
+        } message: {
+            Text("Your final score is \(score)")
         }
     }
     
     func flagtapped(_ number: Int) {
         if number == correctAnswer {
+            addScore(for: 1)
             scoreTitle = "Correct"
+            scoreMessage = "Congratulations! \nCurrent score is: \(score)"
+
         } else {
-            scoreTitle = "Wrong"
+            subtractScore(for: 1)
+            scoreTitle = "Wrong!"
+            scoreMessage = "Thats the flag for \(countries[number]). \nCurrent score is: \(score)"
+
         }
         
+        incrementQuestion()
         showingScore = true
     }
     
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func addScore(for number: Int){
+        score += 1
+    }
+    
+    func subtractScore(for number: Int){
+        if (score - number) < 0 {
+            score = 0
+        } else {
+            score -= 1
+        }
+        
+    }
+    
+    func incrementQuestion() {
+        questionNumber += 1
+        
+        if questionNumber > 8 {
+            showingGameEnd = true
+        }
+    }
+    
+    func resetGame() {
+        questionNumber = 1
+        score = 0
+        scoreTitle = ""
+        scoreMessage = ""
+        showingScore = false
+        showingGameEnd = false
     }
 }
 
