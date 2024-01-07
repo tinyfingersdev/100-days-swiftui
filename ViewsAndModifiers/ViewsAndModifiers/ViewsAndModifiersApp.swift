6adcda1c8f6dc2c55346b163e6ce1fd28cda0338
Shannon Paige <github@bluewhomail.com>
2024-01-07 09:41:06 -0600

    added custom view modifier as part of project 3 part 2

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Shannon Paige on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
