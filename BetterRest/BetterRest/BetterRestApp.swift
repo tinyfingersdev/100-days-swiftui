938443589085bac02352b58bd7dee2bf69360641
Shannon Paige <github@bluewhomail.com>
2024-01-12 19:38:35 -0600

    non working project, skipping days 27 and 28)

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Shannon Paige on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
