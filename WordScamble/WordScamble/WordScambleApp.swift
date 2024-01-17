b864e9f4d10befe42d891220ced7bd5a35240b6b
Shannon Paige <github@bluewhomail.com>
2024-01-17 14:57:04 -0600

    day 30 code

---
+++ WordScamble/WordScamble/WordScambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScambleApp.swift
+//  WordScamble
+//
+//  Created by Shannon Paige on 1/14/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
