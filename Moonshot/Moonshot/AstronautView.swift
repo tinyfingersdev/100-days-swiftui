df3d0744afb9de14da7f3a70a2e2ffce199db38c
Shannon Paige <github@bluewhomail.com>
2024-02-10 15:36:44 -0600

    day 41

---
+++ Moonshot/Moonshot/AstronautView.swift
@@ -0,0 +1,38 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Shannon Paige on 2/10/24.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+                    .padding()
+                
+                
+            }
+            .background(.darkBackground)
+            .navigationTitle(astronaut.name)
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+#Preview {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    return AstronautView(astronaut: astronauts["aldrin"]!)
+        .preferredColorScheme(.dark)
+}
