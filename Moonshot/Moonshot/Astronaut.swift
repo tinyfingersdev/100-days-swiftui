33b313f1b200bb0a5bbf8fef517ca5a8059613fc
Shannon Paige <github@bluewhomail.com>
2024-02-09 20:30:11 -0600

    day 40

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Shannon Paige on 2/6/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
+
